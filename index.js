890acd4d15eff5680ac5c4142faf3294028aecb6
Daniel Wu <1danielbwu1@gmail.com>
2018-07-05 23:28:00 -0600

    Postal Rates

--- index.js
+++ index.js
@@ -8,4 +8,153 @@ const app = express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/hello', (req, res) => res.send('Hello World'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/rates', (req, res) => res.render('pages/rates'))
+  .get('/getRates', getRates)
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+function getRates(req, res) {
+  var weight = req.query.weight;
+  var type = req.query.type;
+  var rate = 0;
+
+  if (weight) {
+    console.log("Weight:", weight);
+    console.log("Weight (ceil):", Math.ceil(weight));
+  } else {
+
+  }
+
+  if (type) {
+    console.log("Type:", type);
+  } else {
+
+  }
+
+  //if (weight && type) {
+    switch(type) {
+      case 1: //Letters (stamped)
+        switch(Math.ceil(weight)) {
+          case 1:
+            rate = 0.5;
+            break;
+          case 2:
+            rate = 0.71;
+            break;
+          case 3:
+            rate = 0.92;
+            break;
+          case 4:
+            rate = 1.13;
+            break;
+          default:
+            rate = 0;
+        }
+        break;
+
+      case 2: //Letters (metered)
+        switch(Math.ceil(weight)) {
+          case 1:
+            rate = 0.47;
+            break;
+          case 2:
+            rate = 0.68;
+            break;
+          case 3:
+            rate = 0.89;
+            break;
+          case 4:
+            rate = 1.10;
+            break;
+          default:
+            rate = 0;
+        }
+        break;
+
+      case 3: //Large Envelopes (flats)
+        switch(Math.ceil(weight)) {
+          case 1:
+            rate = 1.00;
+            break;
+          case 2:
+            rate = 1.21;
+            break;
+          case 3:
+            rate = 1.42;
+            break;
+          case 4:
+            rate = 1.63;
+            break;
+          case 5:
+            rate = 1.84;
+            break;
+          case 6:
+            rate = 2.05;
+            break;
+          case 7:
+            rate = 2.26;
+            break;
+          case 8:
+            rate = 2.47;
+            break;
+          case 9:
+            rate = 2.68;
+            break;
+          case 10:
+            rate = 2.89;
+            break;
+          case 11:
+            rate = 3.10;
+            break;
+          case 12:
+            rate = 3.31;
+            break;
+          case 13:
+            rate = 3.52;
+            break;
+          default:
+            rate = 0;
+        }
+        break;
+
+      case 4: //First-Class Package Service - Retail
+        switch(Math.ceil(weight)) {
+          case 1:
+          case 2:
+          case 3:
+          case 4:
+            rate = 3.50;
+            break;
+          case 5:
+          case 6:
+          case 7:
+          case 8:
+            rate = 3.75;
+            break;
+          case 9:
+            rate = 4.10;
+            break;
+          case 10:
+            rate = 4.45;
+            break;
+          case 11:
+            rate = 4.80;
+            break;
+          case 12:
+            rate = 5.15;
+            break;
+          case 13:
+            rate = 5.50;
+            break;
+          default:
+            rate = 0;
+      }
+      break;
+
+      default:
+        rate = 0;
+    }
+  //}
+
+  console.log("Rate:", rate);
+  res.render('pages/rateResults');
+}
