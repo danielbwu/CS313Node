35fdf1ae99318219ea63dae6b22906002d385dc9
Daniel Wu <1danielbwu1@gmail.com>
2018-07-10 22:31:05 -0600

    test query

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,3 +1,12 @@
+const {Pool} = require('pg');
+const pool = new Pool({
+    user: 'ordrrpumualgtc',
+    host: 'ec2-184-73-199-189.compute-1.amazonaws.com',
+    database: 'dbp22j980k9d0c',
+    password: 'c064fea68415861ee41a4e04d13fa7ba564c31840ecce3a157d7a3d52aa6ecec',
+    port: 5432
+});
+
 //Gets spells
 function getSpells(req, res) {
     console.log("Retrieving spells");
@@ -7,6 +16,13 @@ function getSpells(req, res) {
 //Gets classes
 function getClasses(req, res) {
     console.log("Retrieving classes");
+
+    var text = "SELECT * FROM class"
+    pool.query(text, (err, res) => {
+        console.log(err, res);
+        pool.end();
+    });
+
     res.send("Classes");
 }
 
