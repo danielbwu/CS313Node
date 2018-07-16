fb7047cb78f4c65572373f13562477933bd631a8
Daniel Wu <1danielbwu1@gmail.com>
2018-07-15 23:17:08 -0600

    Fixed looping bug

--- public/scripts/spellBookController.js
+++ public/scripts/spellBookController.js
@@ -21,15 +21,19 @@ app.controller('SpellBookController', ['SpellBookService', '$scope', '$http', fu
 
     //Gets classes for a specific spell
     $scope.getClassesForSpell = function (spell) {
-        console.log("Getting classes for spell:", spell.name);
+        if (!spell.classes) {
+            console.log("Getting classes for spell:", spell.name);
 
-        SpellBookService.getClassesForSpell(spell.id)
-            .then(function (response) {
-                console.log("Classes for ", spell.name, response.data);
-                return response.data;
-            })
-            .catch(function (error) {
-                console.error(error.data);
-            });
+            SpellBookService.getClassesForSpell(spell.id)
+                .then(function (response) {
+                    console.log("Classes for ", spell.name, response.data);
+                    spell.classes = response.data;
+                })
+                .catch(function (error) {
+                    console.error(error.data);
+                });
+        } else {
+            console.log("Classes already retrieved");
+        }
     };
 }]);
